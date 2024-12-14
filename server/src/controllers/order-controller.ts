d62eceb02811fb361eff18713ca1c7f62ed5b88c
skp544 <shubhamkumarprajapati544@gmail.com>
2024-12-15 00:23:30 +0530

    get order successfull api

--- server/src/controllers/order-controller.ts
+++ server/src/controllers/order-controller.ts
@@ -3,6 +3,9 @@ import Order from "@/models/order-model";
 import { BookDoc } from "@/models/book-model";
 import { isValidObjectId } from "mongoose";
 import User from "@/models/user-model";
+import stripe from "@/stripe";
+import { sendErrorResponse } from "@/utils/helper";
+import { StripeCustomer } from "@/stripe/stripe";
 
 export const getOrders: RequestHandler = async (
   req: Request,
@@ -33,6 +36,7 @@ export const getOrders: RequestHandler = async (
             return {
               id: book._id,
               title: book.title,
+              slug: book.slug,
               cover: book.cover?.url,
               price: (price / 100).toFixed(2),
               qty,
@@ -48,7 +52,7 @@ export const getOrders: RequestHandler = async (
 export const getOrderStatus: RequestHandler = async (
   req: Request,
   res: Response
-): Promise<void> => {
+) => {
   const { bookId } = req.params;
 
   let status = false;
@@ -62,4 +66,71 @@ export const getOrderStatus: RequestHandler = async (
   if (user) status = true;
 
   res.json({ status });
+};
+
+export const getOrderSuccessStatus: RequestHandler = async (req, res) => {
+  const { sessionId } = req.body;
+
+  if (typeof sessionId !== "string")
+    return sendErrorResponse({
+      res,
+      message: "Invalid session id!",
+      status: 400,
+    });
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+  const customerId = session.customer;
+
+  let customer: StripeCustomer;
+
+  if (typeof customerId === "string") {
+    customer = (await stripe.customers.retrieve(
+      customerId
+    )) as unknown as StripeCustomer;
+
+    const { orderId } = customer.metadata;
+    const order = await Order.findById(orderId).populate<{
+      orderItems: {
+        id: BookDoc;
+        price: number;
+        qty: number;
+        totalPrice: number;
+      }[];
+    }>("orderItems.id");
+
+    if (!order)
+      return sendErrorResponse({
+        message: "Order not found!",
+        status: 404,
+        res,
+      });
+
+    const data = order.orderItems.map(
+      ({ id: book, price, totalPrice, qty }) => {
+        return {
+          id: book._id,
+          title: book.title,
+          slug: book.slug,
+          cover: book.cover?.url,
+          price: (price / 100).toFixed(2),
+          totalPrice: (totalPrice / 100).toFixed(2),
+          qty,
+        };
+      }
+    );
+
+    res.json({
+      orders: data,
+      totalAmount: order.totalAmount
+        ? (order.totalAmount / 100).toFixed(2)
+        : "0",
+    });
+    return;
+  }
+
+  sendErrorResponse({
+    message: "Something went wrong order not found!",
+    status: 500,
+    res,
+  });
 };
--- server/src/routes/order-route.ts
+++ server/src/routes/order-route.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import {isAuth} from "@/middlewares/auth-middleware";
-import {getOrders, getOrderStatus} from "@/controllers/order-controller";
+import {getOrders, getOrderStatus, getOrderSuccessStatus} from "@/controllers/order-controller";
 
 const router = Router()
 
 router.get("/", isAuth, getOrders)
 router.get("/check-status/:bookId", isAuth, getOrderStatus)
+router.post("/success", isAuth, getOrderSuccessStatus)
 
 export  default  router;
